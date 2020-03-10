0683b9e6237e5d88fb7e8d844ddc6ac9c89e4867
youtapong <youtapong@hotmail.com>
2020-03-10 14:24:46 +0700

    PHP c to f step1

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
-///// php
-<?
 
+<?
+/// convert temperatures in degrees Celsius to Fahrenheit,
+/// (0°C × 9/5) + 32 = 32°F
+
+$clesius = 0;
 
 
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Celsius to Fahrenheit</title>
+</head>
+<body>
+
+<h1>Input Clesius</h1>
+<h2>Input Clesius</h2>
+<p>Fahrenheit is <?php  echo "32 °F" ; ?></p>
+
+</body>
+</html>
